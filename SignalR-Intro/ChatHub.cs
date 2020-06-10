232b226be5146f24efd2f246de9f52b832bb4464
moaz <mohsin.afridi91@gmail.com>
2020-06-10 15:27:05 +0500

    Add project files.

---
+++ SignalR-Intro/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR_Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Intro
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "Explore California", DateTimeOffset.UtcNow, "Hello! What can we help you with today?");
+
+            await base.OnConnectedAsync();
+        }
+        public async Task SendMessage(string name,string text)
+        {
+            var message = new ChatMessage
+            {
+                SenderName = name,
+                Text = text,
+                SendAt = DateTimeOffset.UtcNow
+            };
+
+            // Broadcast to All Clients
+            await Clients.All.SendAsync("ReceiveMessage", message.SenderName, message.SendAt, message.Text);
+
+
+        }
+    }
+}
---
+++ SignalR-Intro/Models/ChatMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Intro.Models
+{
+    public class ChatMessage
+    {
+        public string SenderName { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTimeOffset SendAt { get; set; }
+    }
+}
